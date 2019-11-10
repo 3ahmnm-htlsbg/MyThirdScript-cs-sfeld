4cbe529258ad128ca9f20765309652fa36e88680
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2019-11-10 14:40:58 +0100

    modify AddNumbers script

--- Assets/MyGame/Scripts/AddNumbers.cs
+++ Assets/MyGame/Scripts/AddNumbers.cs
@@ -2,12 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AddNubers : MonoBehaviour
+public class AddNumbers : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        int a = 3;
+
+        int b = 4;
+
+
+
+        Debug.Log("a = " + a);
+
+        Debug.Log("b = " + b);
+
+        Debug.Log("a + b = " + (a + b));
+
+
+
+        Debug.Log("add numbers a = " + a + ", b = " + b + "; a + b = " + (a + b));
     }
 
     // Update is called once per frame

ecaeb57773ddf7414157e3b1d124663bbc4a68d4
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2019-11-10 14:45:51 +0100

    remove unused code

--- Assets/MyGame/Scripts/AddNumbers.cs
+++ Assets/MyGame/Scripts/AddNumbers.cs
@@ -8,25 +8,12 @@ public class AddNumbers : MonoBehaviour
     void Start()
     {
         int a = 3;
-
         int b = 4;
 
-
-
         Debug.Log("a = " + a);
-
         Debug.Log("b = " + b);
-
         Debug.Log("a + b = " + (a + b));
 
-
-
         Debug.Log("add numbers a = " + a + ", b = " + b + "; a + b = " + (a + b));
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
